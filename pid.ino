bef62afcf4d96a58641032f3729d8deed284ea95
vivianzhu88 <31047942+vivianzhu88@users.noreply.github.com>
2019-08-29 21:00:02 -0700

    fix bug for setPointDelta

--- pid.ino
+++ pid.ino
@@ -14,12 +14,11 @@ unsigned long dampMillis;
 const unsigned long period = 1000;  //the value is a number of milliseconds, ie 1 second
 int m_highV = 90;
 int m_lowV = 10;
-int m_period = 4000;
+int m_period = 5000;
 float m_ampFactor = 0.25;
 float m_dampFactor =  0.05;
 float m_dampPeriod = 0.16;
 float m_phaseOffset = 3.75;
-int servoSpeed = 30;
 
 float maxVel = 20.0; // in degrees per second
 float maxAcc = 5.0;  // in degrees per second ^2
@@ -65,6 +64,7 @@ void loop()
     currentMillis = millis();
     float timeDelta = (currentMillis - prevMillis) / 1000.0;
     float dampTimeDelta = (currentMillis - dampMillis) / 1000.0;
+    int setpointDelta;
 
     //figure out where we are in the setpoint waveform
     timeElapsed = currentMillis - startMillis;
@@ -74,7 +74,7 @@ void loop()
     if (prevValue != value) 
     {
       dampMillis = millis();
-      int setpointDelta = value - prevValue; //need to use the amplitude of the change to scale the damping
+      setpointDelta = value - prevValue; //need to use the amplitude of the change to scale the damping
     }
     //calc our current velocity and check that we are not above max
     currVel += maxAcc * timeDelta;
